c6b2e1d8953bb476369aad07ae719a1397b7f97b
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2021-10-18 16:44:42 +0330

    added a workflow to app

--- Program.cs
+++ Program.cs
@@ -13,5 +13,53 @@ __      ____ _| | |_   _
 Console.Clear();
 AnsiConsole.WriteLine(logo);
 
-HdWallpaper h = new("Anime");
-h.MultiDownload();
+var website = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("Which Website to download from?")
+        .PageSize(10)
+        .MoreChoicesText("[grey](Move up and down to reveal more Websites)[/]")
+        .AddChoices(new[] {
+            "WallpapersWide.com",
+            "HdWallpapers.in",
+            "[strikethrough]Pexels.com[/]"
+        }));
+
+string query = AnsiConsole.Ask<string>("What to search for?");
+
+var res = AnsiConsole.Prompt(
+    new TextPrompt<string>("Download single resolution(Single Resolution/Multiple Resolutions)?")
+        .InvalidChoiceMessage("[red]That's not a valid Choice[/]")
+        .DefaultValue("S")
+        .AddChoice("M"));
+
+var random_download = AnsiConsole.Prompt(
+    new TextPrompt<string>("[grey][[Optional]][/] [green]Random or latest(R/L)[/]?")
+        .DefaultValue("L")
+        .InvalidChoiceMessage("[red]That's not a valid Choice[/]")
+        .AllowEmpty());
+bool drandom;
+
+if (random_download == "R")
+    drandom = true;
+else
+    drandom = false;
+
+switch (website)
+{
+    case "WallpapersWide.com":
+        WallpapersWide w = new(query);
+        if (res == "S")
+            w.Download(random: drandom);
+        else
+            w.MultiDownload();
+        break;
+    case "HdWallpapers.in":
+        HdWallpaper h = new(query);
+        if (res == "S")
+            h.Download(random: drandom);
+        else
+            h.MultiDownload();
+        break;
+    default:
+        break;
+}
